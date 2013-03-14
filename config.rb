3d24435ecde753ab9fd9ff670399848ed1418214
Joel Moss <joel@developwithstyle.com>
2013-03-14 08:29:39 +0000

    Build!

--- config.rb
+++ config.rb
@@ -130,7 +130,7 @@ configure :build do
   activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   activate :gzip
   activate :asset_hash

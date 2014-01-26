9c180c38c099a8156cc7af9f4ac0585b4c5dc36e
Joel Moss <joel@developwithstyle.com>
2014-01-26 23:00:21 +0000

    Using correct hostname

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,2 @@
 set :stage, :staging
-
-server 'ec2-54-198-198-236.compute-1.amazonaws.com', user: 'deploy', roles: %w{web app db}
+server 'web1.int.codio.com', user: 'deploy', roles: %w{web app db}

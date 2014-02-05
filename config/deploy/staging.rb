dff305a7f09302596cba77762a295497b5e8c939
Joel Moss <joel@developwithstyle.com>
2014-02-05 13:37:36 +0000

    Created test stage

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,3 +2,7 @@ set :stage, :staging
 set :deploy_to, '/home/middleman/codio-site'
 
 server 'ec2-184-72-144-200.compute-1.amazonaws.com', user: 'middleman', roles: %w{web app db}
+
+
+Rake::Task["bundler:map_bins"].clear
+Rake::Task["bundler:install"].clear

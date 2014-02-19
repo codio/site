4b666dcacc14bf620819fabb5733d8c06de9252d
Freddy May <fmay@applicationcraft.com>
2014-02-19 09:30:13 +0100

    pricing mods edu

--- build/js/features-6c6b05a1.js
+++
@@ -1,1 +0,0 @@
-(function(){$(function(){return $(".feature-list ul li a").on("click",function(){var e,t,i;return e=$(this),t=$("#feature-video iframe"),i=t.attr("src").replace(/[0-9]+/,e.attr("href").replace("#","")||"123"),t.attr("src",i),$(".feature-list li a").removeClass("active"),e.addClass("active"),!1})})}).call(this);

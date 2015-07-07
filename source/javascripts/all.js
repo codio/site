80f68c404b027fa86b97c3b5ce2e252f3e91412a
dignifiedquire <dignifiedquire@gmail.com>
2015-07-07 15:59:20 +0200

    Install wow.js

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,7 +2,10 @@
 //= require cookie-monster
 //= require blueimp-md5
 //= require dragdealer
+//= require wow.js/dist/wow.js
 //
 //= require_tree ./vendors
 //
 //= require_tree ./partials
+
+new WOW().init()
